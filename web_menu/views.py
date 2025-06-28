6871a80b600a6067646640a5df3a7a7de04104e9
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2025-06-28 08:45:24 +0330

    Context 56

--- web_menu/views.py
+++ web_menu/views.py
@@ -9,6 +9,14 @@ class MenuList(generic.ListView):
     queryset = Items.objects.order_by("-date_created")    # put - to reverse
     template_name = "index.html"
 
+    def get_context_data(self):
+        context = {
+            "meals": "Pizza",
+            "price": 12.22 
+        }
+        # in index.html file use {{ key }} to get the value
+        return context
+
 
 class MenuItemDetail(generic.DetailView):
     model = Items
